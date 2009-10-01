3f67d62ff4dd8c3af58aa9cc974f2e3c61372f2c
Robin Debreuil <robin@debreuil.com>
2009-09-30 19:33:41 -0500

    added custom rect

---
+++ VexRuntime/V2D/Serialization/V2DRectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VexRuntime.V2D
+{
+    public struct V2DRectangle
+    {
+        public float X;
+        public float Y;
+        public float Width;
+        public float Height;
+
+        public V2DRectangle(float x, float y, float width, float height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
